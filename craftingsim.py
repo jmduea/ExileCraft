2e9715144bbaf27ea5ba028a0dabfa3a3a7cbd47
Jon Duea <119540897+jmduea@users.noreply.github.com>
2023-04-29 04:54:24 -0500

    added function to open crafting dialog

--- craftingsim.py
+++ craftingsim.py
@@ -1,167 +1,196 @@
-from numpy import random
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTextEdit, QPushButton, QMessageBox
 
 
 class CraftingSimulator:
-    def __init__(self, item):
-        self.item = item
-        self.mods = []
+    def __init__(self, db_handler):
+        self.db_handler = db_handler
 
-        # Function to upgrade a normal item to a magic item
-        def apply_transmutation_orb(self, game_data):
-            # Checks that the items rarity is normal
-            if self.rarity == 'normal':
-                # Changes the rarity to magic
-                self.rarity = 'magic'
-                # Calls the apply_random_mods function to add 1-2 random mods to the item
-                self.apply_random_mods(game_data, 1, 2)
 
-        # Function to reforge a magic item with new random properties
-        def apply_alteration_orb(self, game_data):
-            # Checks that the items rarity is magic
-            if self.rarity == 'magic':
-                # Clears the mods on the item
-                self.mods.clear()
-                # Calls the apply_random_mods function to add 1-2 random mods to the item
-                self.apply_random_mods(game_data, 1, 2)
+    def open_crafting_project_dialog(self):
+        dialog = QDialog()
+        dialog.setWindowTitle('Import Crafting Project')
 
-        # Function to augment a magic item with a new random property
-        def apply_augmentation_orb(self, game_data):
-            # Checks that the item rarity is magic and if the item has less than two mods
-            if self.rarity == 'magic' and len(self.mods) < 2:
-                # Calls the apply_random_mods function to add one new random mod to the item
-                self.apply_random_mods(game_data, 1, 1)
+        layout = QVBoxLayout()
+        text_edit = QTextEdit()
+        layout.addWidget(text_edit)
 
-        # Function to upgrade a normal item to a rare item
-        def apply_alchemy_orb(self, game_data):
-            pass
+        save_button = QPushButton('Save')
+        save_button.clicked.connect(lambda: self.save_crafting_project(text_edit.toPlainText(), dialog))
+        layout.addWidget(save_button)
+        dialog.setLayout(layout)
+        dialog.setWindowOpacity(1)
+        text_edit.setWindowOpacity(1)
+        dialog.exec_()
 
-        # Function to reforge a rare item with new random modifiers
-        def apply_chaos_orb(self, game_data):
-            pass
+    def save_crafting_project(self, data, dialog):
+        # Process the pasted data and save it to the crafting_projects table.
+        # You may need to create additional methods in your DatabaseHandler class to insert the data.
+        success = self.db_handler.insert_crafting_project(data)
 
-        # Function to enchant a rare item with a new random property
-        def apply_exalted_orb(self, game_data):
-            pass
+        if success:
+            QMessageBox.information(dialog, 'Success', 'Crafting project saved successfully.')
+            dialog.close()
+        else:
+            QMessageBox.critical(dialog, 'Error', 'Failed to save crafting project.')
 
-        # Function to remove all properties from an item
-        def apply_scouring_orb(self, game_data):
-            pass
 
-        # Function to remove a random modifier from an item
-        def apply_annulment_orb(self, game_data):
-            pass
 
-        # Function to enchant a rare item with a new crusader modifier
-        def apply_crusader_orb(self, game_data):
-            pass
-
-        # Function to enchant a rare item with a new hunter modifier
-        def apply_hunter_orb(self, game_data):
-            pass
-
-        # Function to enchant a rare item with a new redeemer modifier
-        def apply_redeemer_orb(self, game_data):
-            pass
-
-        # Function to enchant a rare item with a new warlord modifier
-        def apply_warlord_orb(self, game_data):
-            pass
-
-        # Function to randomise the numeric values of the implicit properties of an item
-        def apply_blessed_orb(self, game_data):
-            pass
-
-        # Function to randomise the numeric values of the random properties on an item
-        def apply_divine_orb(self, game_data):
-            pass
-
-        # Function to reforge a rare item with new random modifiers, including a veiled modifier
-        def apply_veiled_chaos_orb(self, game_data):
-            pass
-
-        # Function to destroy an item, applying its influence to another of the same item class.
-        # The second item is reforged as a rare item with both influence types and new modifiers
-        def apply_awakener_orb(self, game_data):
-            pass
-
-        # Function to remove one influenced modifier from an item with at least two influenced modifiers
-        # and upgrades another influenced modifier
-        def apply_maven_orb(self, game_data):
-            pass
-
-        # Function to fracture a random modifier on a rare item with at least 4 modifiers, locking it in place
-        def apply_fracturing_orb(self, game_data):
-            pass
-
-        # Function to corrupt an item, modifying it unpredictably
-        def apply_vaal_orb(self, game_data):
-            pass
-
-        # Function to apply a fossil craft using 1-4 fossils selected from a list of fossils
-        def fossil_craft(self, game_data):
-            pass
-
-        # Function to apply a harvest craft selected from a list of harvest crafts
-        def harvest_craft(self, game_data):
-            pass
-
-        # Function to apply an essence craft selected from a list of essence crafts
-        def essence_craft(self, game_data):
-            pass
-
-        # Function to apply an eldritch craft selected from a list of eldritch crafts
-        def eldritch_craft(self, game_data):
-            pass
-
-        # Function to potently corrupt an item, modifying it drastically and unpredictably
-        def double_corrupt(self, game_data):
-            pass
-
-        # Function to apply a syndicate craft selected from a list of syndicate crafts
-        def syndicate_craft(self, game_data):
-            pass
-
-        # Function to add random modifiers to an item
-        # TODO: add accounting for mod weights, item tags, and mod groups
-        def apply_random_mods(self, game_data, min_mods, max_mods):
-            available_mods = [mod for mod in game_data.mods.values() if mod.domain == self.base_item.domain and
-                              mod.required_level <= self.item_level]  # Filters based on item domain and item level
-            num_mods = random.randint(min_mods, max_mods)
-            # Adds a random number of mods within the range
-            for _ in range(num_mods):
-                mod = random.choice(available_mods)
-                self.mods.append(mod)
-
-        def add_mod(self, mod):
-            if len(self.mods) < self.item.max_mods and mod not in self.mods:
-                self.mods.append(mod)
-
-        def remove_mod(self, mod):
-            if mod in self.mods:
-                self.mods.remove(mod)
-
-        def remove_all_affixes(self):
-            self.mods = [mod for mod in self.mods if mod.generation_type not in ['prefix', 'suffix']]
-
-        def upgrade_quality(self, amount):
-            new_quality = self.item.quality + amount
-            self.item.quality = min(new_quality, 20)
-
-        def change_sockets(self, socket_data):
-            # Change the sockets of the item according to the given data
-            pass
-
-        def craft_with_essence(self, essence):
-            # Apply the essence to the item
-            pass
-
-        def craft_with_fossil(self, fossil):
-            # Apply the fossil to the item
-            pass
-
-        def reroll_mods(self):
-            # Reroll the mods on the item
-            pass
-
-        def __repr__(self):
-            return f"CraftingSimulator(item={self.item}, mods={self.mods})"
+    # # Function to upgrade a normal item to a magic item
+    #     def apply_transmutation_orb(self):
+    #         # Checks that the items rarity is normal
+    #         if self.rarity == 'normal':
+    #             # Changes the rarity to magic
+    #             self.rarity = 'magic'
+    #             # Calls the apply_random_mods function to add 1-2 random mods to the item
+    #             self.apply_random_mods(game_data, 1, 2)
+    #
+    #     # Function to reforge a magic item with new random properties
+    #     def apply_alteration_orb(self, game_data):
+    #         # Checks that the items rarity is magic
+    #         if self.rarity == 'magic':
+    #             # Clears the mods on the item
+    #             self.mods.clear()
+    #             # Calls the apply_random_mods function to add 1-2 random mods to the item
+    #             self.apply_random_mods(game_data, 1, 2)
+    #
+    #     # Function to augment a magic item with a new random property
+    #     def apply_augmentation_orb(self, game_data):
+    #         # Checks that the item rarity is magic and if the item has less than two mods
+    #         if self.rarity == 'magic' and len(self.mods) < 2:
+    #             # Calls the apply_random_mods function to add one new random mod to the item
+    #             self.apply_random_mods(game_data, 1, 1)
+    #
+    #     # Function to upgrade a normal item to a rare item
+    #     def apply_alchemy_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to reforge a rare item with new random modifiers
+    #     def apply_chaos_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to enchant a rare item with a new random property
+    #     def apply_exalted_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to remove all properties from an item
+    #     def apply_scouring_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to remove a random modifier from an item
+    #     def apply_annulment_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to enchant a rare item with a new crusader modifier
+    #     def apply_crusader_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to enchant a rare item with a new hunter modifier
+    #     def apply_hunter_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to enchant a rare item with a new redeemer modifier
+    #     def apply_redeemer_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to enchant a rare item with a new warlord modifier
+    #     def apply_warlord_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to randomise the numeric values of the implicit properties of an item
+    #     def apply_blessed_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to randomise the numeric values of the random properties on an item
+    #     def apply_divine_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to reforge a rare item with new random modifiers, including a veiled modifier
+    #     def apply_veiled_chaos_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to destroy an item, applying its influence to another of the same item class.
+    #     # The second item is reforged as a rare item with both influence types and new modifiers
+    #     def apply_awakener_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to remove one influenced modifier from an item with at least two influenced modifiers
+    #     # and upgrades another influenced modifier
+    #     def apply_maven_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to fracture a random modifier on a rare item with at least 4 modifiers, locking it in place
+    #     def apply_fracturing_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to corrupt an item, modifying it unpredictably
+    #     def apply_vaal_orb(self, game_data):
+    #         pass
+    #
+    #     # Function to apply a fossil craft using 1-4 fossils selected from a list of fossils
+    #     def fossil_craft(self, game_data):
+    #         pass
+    #
+    #     # Function to apply a harvest craft selected from a list of harvest crafts
+    #     def harvest_craft(self, game_data):
+    #         pass
+    #
+    #     # Function to apply an essence craft selected from a list of essence crafts
+    #     def essence_craft(self, game_data):
+    #         pass
+    #
+    #     # Function to apply an eldritch craft selected from a list of eldritch crafts
+    #     def eldritch_craft(self, game_data):
+    #         pass
+    #
+    #     # Function to potently corrupt an item, modifying it drastically and unpredictably
+    #     def double_corrupt(self, game_data):
+    #         pass
+    #
+    #     # Function to apply a syndicate craft selected from a list of syndicate crafts
+    #     def syndicate_craft(self, game_data):
+    #         pass
+    #
+    #     # Function to add random modifiers to an item
+    #     # TODO: add accounting for mod weights, item tags, and mod groups
+    #     def apply_random_mods(self, game_data, min_mods, max_mods):
+    #         available_mods = [mod for mod in game_data.mods.values() if mod.domain == self.base_item.domain and
+    #                           mod.required_level <= self.item_level]  # Filters based on item domain and item level
+    #         num_mods = random.randint(min_mods, max_mods)
+    #         # Adds a random number of mods within the range
+    #         for _ in range(num_mods):
+    #             mod = random.choice(available_mods)
+    #             self.mods.append(mod)
+    #
+    #     def add_mod(self, mod):
+    #         if len(self.mods) < self.item.max_mods and mod not in self.mods:
+    #             self.mods.append(mod)
+    #
+    #     def remove_mod(self, mod):
+    #         if mod in self.mods:
+    #             self.mods.remove(mod)
+    #
+    #     def remove_all_affixes(self):
+    #         self.mods = [mod for mod in self.mods if mod.generation_type not in ['prefix', 'suffix']]
+    #
+    #     def upgrade_quality(self, amount):
+    #         new_quality = self.item.quality + amount
+    #         self.item.quality = min(new_quality, 20)
+    #
+    #     def change_sockets(self, socket_data):
+    #         # Change the sockets of the item according to the given data
+    #         pass
+    #
+    #     def craft_with_essence(self, essence):
+    #         # Apply the essence to the item
+    #         pass
+    #
+    #     def craft_with_fossil(self, fossil):
+    #         # Apply the fossil to the item
+    #         pass
+    #
+    #     def reroll_mods(self):
+    #         # Reroll the mods on the item
+    #         pass
+    #
+    #     def __repr__(self):
+    #         return f"CraftingSimulator(item={self.item}, mods={self.mods})"
