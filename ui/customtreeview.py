7d7a704ced1c18542b45421e417d09f535efdd3b
Jon Duea <119540897+jmduea@users.noreply.github.com>
2023-04-30 05:01:46 -0500

    add CustomTreeView class

---
+++ ui/customtreeview.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTreeView, QHeaderView
+
+
+class CustomTreeView(QTreeView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setHeaderHidden(False)
+        self.setStyleSheet("""
+            QTreeView {
+                color: "white";
+            }
+            QWidget {
+                background-color: #282828;
+                border: 0px;
+            }
+            QLabel {
+                color: #8888ff;
+                border: 0px;
+                margin: 0;
+                padding: 0;
+                font-size: 16px;
+            }
+        """)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.header().setSizeAdjustPolicy(QHeaderView.AdjustToContents)
