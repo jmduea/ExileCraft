18cb7f5e10ec65369929f94fe8e539c2c4bbccf3
jmduea <jmduea@gmail.com>
2023-07-28 23:56:46 -0500

    update base model to include TableNameMixIn

--- ExileCraft/modules/data/models/base_model.py
+++ ExileCraft/modules/data/models/base_model.py
@@ -21,63 +21,89 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 # ##############################################################################
-import math
-import re
 from dataclasses import dataclass
 from typing import Annotated, Optional
 
+from sqlalchemy import Column
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, mapped_column, declared_attr, Mapped, relationship
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column, MappedAsDataclass, relationship
+
+from modules.shared.config.global_functions import snake_case
 
 intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
 str50 = Annotated[str, mapped_column(String(50), default=None)]
 
 
-@dataclass
-class Base(MappedAsDataclass, DeclarativeBase):
-    pass
-
-
 class TableNameMixIn:
     """
     Provides a method for generating the name of a database table based on the name of the class.
-    
+
     Uses regular expressions to convert the class name from CamelCase to snake_case and appends an 's' to the end of
     the resulting string to form the table name.
-    
+
     Methods:
     -__tablename__(cls) -> Optional[str]:
         Class method that returns the name of the database table that corresponds to the class that uses the
         TableNameMixin mixin. Uses regular expressions to convert the class name from CamelCase to snake_case.
-    
+
     """
-    
+
+    # noinspection PyMethodParameters
     @declared_attr
     def __tablename__(cls) -> Optional[str]:
-        name = cls.__name__
-        name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-        name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
-        return name + 's'
-    
-    
+        name = snake_case(cls.__name__)
+        return name
+
+
+@dataclass
+class Base(MappedAsDataclass, DeclarativeBase, TableNameMixIn):
+    """
+    Base class for all database models.
+
+    Provides a primary key column named 'id' that is an auto-incrementing integer.
+
+    Attributes:
+
+     id: Mapped[intpk] = mapped_column(init=False):
+
+      Primary key column that is an auto-incrementing integer.
+    """
+    id: Mapped[intpk] = mapped_column(init=False)
+
+
+# noinspection PyMethodParameters
+@dataclass
+class ItemPropertyMixin:
+    @declared_attr
+    def item_id(cls):
+        return Column(Integer, ForeignKey('item.id'), nullable=False)
+
+    @declared_attr
+    def item(cls):
+        return relationship('Item', init=False,
+                            backref=snake_case(cls.__name__))
+
+
+# noinspection PyMethodParameters
 @dataclass
 class ItemClassMixin(MappedAsDataclass):
-    item_class_id: Mapped[int] = mapped_column("ItemClass", ForeignKey('item_classes.id'))
-    
+    item_class_id: Mapped[int] = mapped_column("item_class_id", ForeignKey('item_class.id'))
+
     @declared_attr
-    def item_class(cls) -> Mapped["ItemClass"]:
+    def item_class(cls):
         return relationship('ItemClass', init=False)
 
 
+# noinspection PyMethodParameters
 @dataclass
 class ItemClassSubtypeMixin(MappedAsDataclass):
     item_class_subtype_id: Mapped[Optional[int]] = mapped_column("ItemClassSubtype", ForeignKey(
-        'item_class_subtypes.id'),
+        'item_class_subtype.id'),
                                                                                 init=False)
-    
+
     @declared_attr
-    def item_class_subtype(cls) -> Mapped["ItemClassSubtype"]:
+    def item_class_subtype(cls):
         return relationship('ItemClassSubtype', init=False)
 
 
@@ -93,7 +119,7 @@ class InfluencedItemMixin(MappedAsDataclass):
     is_eater_of_worlds_item: Mapped[bool] = mapped_column(deferred=True, deferred_group='influence_flags')
     is_searing_exarch_item: Mapped[bool] = mapped_column(deferred=True, deferred_group='influence_flags')
     is_synthesis_item: Mapped[bool] = mapped_column(deferred=True, deferred_group='influence_flags')
-    
+
 
 @dataclass
 class FlaskPropertiesMixin(MappedAsDataclass):
