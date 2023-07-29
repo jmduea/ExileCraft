b9e34b3912492901230b7cb4485441e277f12348
jmduea <jmduea@gmail.com>
2023-07-29 00:41:54 -0500

    Refactor fossil data parsing method
    The commit simplifies and extracts the fossil data parsing process by removing code that inserts data into a database from the 'fossils_parser.py' module. The removed functions were closely tied to database operations, making the parser module less flexible. The new approach creates a dedicated 'FossilParser' class that provides a static method 'parse_json' for parsing fossil data from a JSON file, giving back the flexibility to the module.

--- ExileCraft/modules/data/parser/fossils_parser.py
+++ ExileCraft/modules/data/parser/fossils_parser.py
@@ -25,9 +25,6 @@
 import json
 import os
 
-from modules.data.models.fossil_models import Fossils
-from modules.data.models.tag_models import Tags
-
 from modules.data.parser.path_utils import get_abs_path, get_base_dir
 
 script_path = os.path.realpath(__file__)
@@ -35,66 +32,9 @@ base_dir = get_base_dir(script_path)
 abs_path_to_json = get_abs_path(base_dir, os.path.join(r'ExileCraft\modules\data', 'json', 'fossils.min.json'))
 
 
-def insert_fossils_into_db(db_manager):
-    with open(abs_path_to_json) as json_file:
-        fossils_data = json.load(json_file)
-        
-    with db_manager.transaction() as session:
-        for fossil_id, fossil_info in fossils_data.items():
-            fossil = Fossils(fossil_id=fossil_id,
-                             changes_quality=fossil_info['changes_quality'],
-                             enchants=fossil_info['enchants'],
-                             mirrors=fossil_info['mirrors'],
-                             name=fossil_info['name'],
-                             rolls_lucky=fossil_info['rolls_lucky'],
-                             rolls_white_sockets=fossil_info['rolls_white_sockets']
-                             )
-            session.add(fossil)
-            
-        for tag in fossil_info['allowed_tags']:
-            tag_instance = session.query(Tags).get(tag)
-            if not tag_instance:
-                tag_instance = Tags(tag_id=tag)
-                session.add(tag_instance)
-            
-            allowed_tag = FossilAllowedTags(fossil_id=fossil.id, tag_id=tag_instance.id)
-            session.add(allowed_tag)
-        
-        for tag in fossil_info['forbidden_tags']:
-            # First, make sure the tag exists in the Tags table
-            tag_instance = session.query(Tags).get(tag)
-            if not tag_instance:
-                tag_instance = Tags(tag_id=tag)
-                session.add(tag_instance)
-            
-            forbidden_tag = FossilForbiddenTags(fossil_id=fossil.id, tag_id=tag_instance.id)
-            session.add(forbidden_tag)
-        
-        for mod_weight in fossil_info['positive_mod_weights']:
-            tag = mod_weight['tag']
-            weight = mod_weight['weight']
-            
-            # First, make sure the tag exists in the Tags table
-            tag_instance = session.query(Tags).get(tag)
-            if not tag_instance:
-                tag_instance = Tags(tag_id=tag)
-                session.add(tag_instance)
-            
-            positive_mod_weight = FossilPositiveModWeights(fossil_id=fossil.fossil_id, tag_id=tag_instance.tag_id,
-                                                           weight=weight)
-            session.add(positive_mod_weight)
-        
-        for mod_weight in fossil_info['negative_mod_weights']:
-            tag = mod_weight['tag']
-            weight = mod_weight['weight']
-            
-            # First, make sure the tag exists in the Tags table
-            tag_instance = session.query(Tags).get(tag)
-            if not tag_instance:
-                tag_instance = Tags(tag_id=tag)
-                session.add(tag_instance)
-            
-            negative_mod_weight = FossilNegativeModWeights(fossil_id=fossil.fossil_id, tag_id=tag_instance.tag_id,
-                                                           weight=weight)
-            session.add(negative_mod_weight)
-            
+class FossilParser:
+    @staticmethod
+    def parse_json():
+        with open(abs_path_to_json) as json_file:
+            data = json.load(json_file)
+        return data
