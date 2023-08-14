d13cf7577c30edb725755d545a2dc82aa7ab7a7f
jmduea <jmduea@gmail.com>
2023-08-14 07:56:29 -0500

    Refactor module imports and remove unused class

--- ExileCraft/modules/__init__.py
+++ ExileCraft/modules/__init__.py
@@ -1,17 +1,5 @@
-from .data.database_manager import DatabaseManager
-from .data.database_manager import ItemManager
-from .data.database_manager import ModManager
-from .data.models import item_models
-from .data.models import mod_models
-from .data.models import base_model
-from .data.models import association_models
-from .data.models import shared_models
-from .data.models import stat_models
-from .data.models import tag_models
-from .data.models import translation_models
+from .shared.global_cache import GlobalCache
+from .sim.crafting import CraftingManager
 
-
-db_manager = DatabaseManager()
-mod_manager = ModManager()
-item_manager = ItemManager()
-session = db_manager.get_session()
+CACHE = GlobalCache()
+CRAFTING_MANAGER = CraftingManager(item=None)
